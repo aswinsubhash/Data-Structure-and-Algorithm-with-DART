6e30b5d03bb29bfbfa4643620e98f7444b8fd5da
Aswin S <aswinofficial3@gmail.com>
2023-10-14 17:56:47 +0530

    Get in Linked list

---
+++ bin/Linked List/get.dart
@@ -0,0 +1,61 @@
+class Node {
+  final int value;
+  Node? next;
+
+  Node({
+    required this.value,
+  });
+}
+
+class LinkedList {
+  Node? head;
+  Node? tail;
+  int? length;
+
+  LinkedList(int value) {
+    Node newNode = Node(value: value);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+  }
+
+  void append(int value) {
+    Node newNode = Node(value: value);
+
+    if (head == null) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail?.next = newNode;
+      tail = newNode;
+    }
+
+    length = (length ?? 0) + 1;
+  }
+
+  Node? get(int index) {
+    if (index < 0 || index >= (length ?? 0)) return null;
+    Node? temp = head;
+    for(int i = 0; i < index; i++){
+      temp = temp?.next;
+    }
+    return temp;
+  }
+
+  void printLinkedList() {
+    Node? temp = head;
+
+    while (temp != null) {
+      print(temp.value);
+      temp = temp.next;
+    }
+  }
+}
+
+void main() {
+  LinkedList linkedList = LinkedList(1);
+  linkedList.append(4);
+  linkedList.append(5);
+  linkedList.printLinkedList();
+  print('When get applied: ${linkedList.get(1)?.value}');
+}
