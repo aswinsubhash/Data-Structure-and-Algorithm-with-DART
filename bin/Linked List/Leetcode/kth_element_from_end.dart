69cc2f9928af9c41c323095fc72d560981c23af8
Aswin S <aswinofficial3@gmail.com>
2023-10-15 13:46:00 +0530

    Kth Element from end in a linked list

--- bin/Linked List/Leetcode/kth_element_from_end.dart
+++ bin/Linked List/Leetcode/kth_element_from_end.dart
@@ -1,4 +1,31 @@
+/// [Find Kth Node From End] ( ** Interview Question)
 
+// Implement a method called findKthFromEnd that returns the k-th node from the end of the list.
+// If the list has fewer than k nodes, the method should return null.
+
+// Note: This implementation of the Linked List class does not have the length attribute.
+
+// Method signature:
+
+// public Node findKthFromEnd(int k)
+
+// Example:
+
+// LinkedList myList = new LinkedList(1);
+// myList.append(2);
+// myList.append(3);
+// myList.append(4);
+// myList.append(5);
+
+// Node result = myList.findKthFromEnd(2); // Output: Node with value 4
+
+// result = myList.findKthFromEnd(5); // Output: Node with value 1
+
+// result = myList.findKthFromEnd(6); // Output: null
+
+// Note:
+
+// In this problem, you should use the two-pointer technique to efficiently find the k-th node from the end of the linked list.
 
 class Node {
   final int value;
@@ -39,7 +66,6 @@ class LinkedList {
     Node? fast = head;
 
     for (int i = 0; i < k; i++) {
-      
       if (fast == null) return null;
 
       fast = fast.next;
@@ -69,5 +95,5 @@ void main() {
   linkedList.append(5);
   linkedList.append(6);
   linkedList.printLinkedList();
+  print('Kth Element from the last is: ${linkedList.findKthFromEnd(2)?.value}');
 }
-
