893e7bf40fec2292325f19a585aaed8e21eeae65
Aswin S <aswinofficial3@gmail.com>
2023-10-24 19:59:06 +0530

    Recursive insert

--- bin/Data Structures/Trees/BinarySearchTree/RescursiveBST/insert.dart
+++ bin/Data Structures/Trees/BinarySearchTree/RescursiveBST/insert.dart
@@ -9,15 +9,20 @@ class Node {
 
 class BinarySearchTree {
   Node? root;
-
   Node rInsert(Node? currentNode, int value) {
+    // Base case: If the current node is null, create a new node with the given value.
     if (currentNode == null) return Node(value: value);
 
+    // If the value to be inserted is less than the current node's value, go to the left subtree.
     if (value < currentNode.value) {
       currentNode.left = rInsert(currentNode.left, value);
-    } else if (value > currentNode.value) {
+    }
+    // If the value to be inserted is greater than the current node's value, go to the right subtree.
+    else if (value > currentNode.value) {
       currentNode.right = rInsert(currentNode.right, value);
     }
+
+    // Return the current node, which may have been updated with new child nodes.
     return currentNode;
   }
 
