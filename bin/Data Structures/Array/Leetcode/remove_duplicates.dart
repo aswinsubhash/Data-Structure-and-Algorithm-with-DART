0fba491d2af550b4237392d0d34b5e00335e38e6
Aswin S <aswinofficial3@gmail.com>
2023-10-27 17:45:54 +0530

    Remove duplicates in an array

--- bin/Data Structures/Array/Leetcode/remove_duplicates.dart
+++ bin/Data Structures/Array/Leetcode/remove_duplicates.dart
@@ -1,3 +1,12 @@
+/// Array: [Remove Duplicate]s ( ** Interview Question)
+/// The task is to write a method named removeDuplicates, 
+/// that removes all duplicate occurrences of integers from a sorted array in-place such that each unique element appears only once.
+/// The relative order of the elements should be kept the same.
+/// The method also needs to return the new length of the array after duplicates have been removed.
+///  It's important to note that the array is passed by reference to the function, 
+/// which means modification to the array will be known to the caller as well.
+
+
 int removeDuplicates(List<int> nums) {
   if (nums.isEmpty) {
     return 0; // Empty array has no duplicates.
